30cd08e7d096465883fe269fe47a320c7bf8519e
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-04-09 22:59:13 +0300

    last commit

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 "use strict";
-class Menu{
-    constructor(name, description, price, imgUrl){
+class Menu {
+    constructor(name, description, price, imgUrl) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
     }
-    display(generalLayout){
-
+    display(generalLayout) {
         const layout = document.createElement("div");
-        layout.style.display = "block";
-        layout.style.borderRadius = "15px";
+        layout.style.boxShadow = "10px 5px black";
+        generalLayout.append(layout);
         const img = document.createElement("img");
-        img.style.background = this.imgUrl;
+        img.src = this.imgUrl;
         img.style.width = "100%";
         img.style.height = "40%";
         img.style.display = "block";
         layout.append(img);
         const texts = document.createElement("div");
         texts.style.width = "100%";
-        texts.style.height = "50%";
+        texts.style.height = "60%";
         layout.append(texts);
         const title = document.createElement("h3");
         title.innerHTML = `Menu: ${this.name}`;
@@ -28,12 +27,10 @@ class Menu{
         const description = document.createElement("p");
         description.innerHTML = this.description;
         texts.append(description);
-
-        generalLayout.append(layout);
     }
 }
 const Fitnes = new Menu("Fitnes", "Lorem ipsum dolor si ammetLorem ipsum dolor" +
     " si ammetLorem ipsum dolor si ammetLorem ipsum dolor si ammetLorem ipsum " +
-    "dolor si ammetLorem ipsum dolor si ammetLorem ipsum dolor si ammetLorem " + 
+    "dolor si ammetLorem ipsum dolor si ammetLorem ipsum dolor si ammetLorem " +
     "ipsum dolor si ammetLorem ipsum dolor si ammet", 229, "1.jpg");
 Fitnes.display(document.body);
